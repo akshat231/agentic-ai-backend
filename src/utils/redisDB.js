d6416e5b182a43eaf4dfe838afdc255c54dab08e
akshat231 <akshatisthegreat@gmail.com>
2025-04-11 00:51:26 +0530

    made redis dynamic

--- src/utils/redisDB.js
+++ src/utils/redisDB.js
@@ -1,15 +1,17 @@
 const Redis = require('ioredis');
 const logger = require('./logger');
 
-// Connect to Redis running on localhost:6379
-const redis = new Redis(); 
+// Use environment variable or fallback for local dev
+const redisUrl = process.env.REDIS_URL || 'redis://localhost:6379';
+
+const redis = new Redis(redisUrl);
 
 redis.on('connect', () => {
   logger.info('Connected to Redis');
 });
 
 redis.on('error', (err) => {
-    logger.error('Redis error:', err);
+  logger.error('Redis error:', err);
 });
 
 module.exports = redis;
